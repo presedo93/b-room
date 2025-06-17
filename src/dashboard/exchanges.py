ee3bde5615dddbbec5b59c27a003563e750c011f
Rene Presedo <r.presedo@Renes-MacBook-Air.local>
2025-06-17 21:13:26 +0200

    fix: simplify streamlit structure for the moment

--- src/dashboard/exchanges.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-from dashboard.shared import header_buttons
-
-
-def page():
-    header_buttons()
-
-    st.write("EXCHANGES PAGE: This page is under construction.")
--- src/main.py
+++ src/main.py
@@ -3,22 +3,42 @@ import streamlit as st
 from sqlmodel import create_engine
 from loguru import logger
 
-from dashboard.exchanges import page as exc_page
-from dashboard.backtester import page as bck_page
-
 logger.add("xini.log", retention="2 days")
 logger.info("Dashboard started")
 
 engine = create_engine("sqlite:///xini.db")
 
+
+def header_buttons():
+    c1, _, c3 = st.columns(3)
+
+    with c1:
+        st.page_link(exc_page, label="exchanges", icon="🔀", use_container_width=True)
+
+    with c3:
+        st.page_link(bck_page, label="b-room", icon="🧠", use_container_width=True)
+
+
+def backtester():
+    st.title("Backtester")
+
+    header_buttons()
+
+    st.write("This is the page where the magic happens.")
+
+
+def exchanges():
+    st.title("Exchanges")
+
+    header_buttons()
+
+    st.write("This page shows data extracted from crypto exchanges.")
+
+
+exc_page = st.Page(exchanges, icon="🔀")
+bck_page = st.Page(backtester, icon="🧠")
+
 if __name__ == "__main__":
     st.set_page_config(initial_sidebar_state="collapsed")
 
-    pg = st.navigation(
-        [
-            st.Page(exc_page, title="exchanges", icon="🥇", url_path="exchanges/"),
-            st.Page(bck_page, title="backtester", icon="🥈"),
-        ],
-    )
-
-    pg.run()
+    st.navigation([exc_page, bck_page], position="hidden").run()
